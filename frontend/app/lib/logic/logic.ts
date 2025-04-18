b31e1c7ec37ade6e58b5a1cb6289645f09aefba7
Justin Ottesen <justinottesen@gmail.com>
2025-04-17 21:42:13 -0400

    Types Improvements
    Another Murray commit, again merge conflicts

--- frontend/app/lib/logic/logic.ts
+++ frontend/app/lib/logic/logic.ts
@@ -1,89 +1,91 @@
 // === Term-level: Constants and Variables ===
 
+import exp from "constants";
+
 export type Constant = {
   type: "constant";
   name: string; // e.g., "alice", "a"
 };
-
 export type Variable = {
   type: "variable";
   name: string; // e.g., "x", "y"
 };
-
 // A Term is either a constant or a variable
 export type Term = Constant | Variable;
-
 // === Predicate ===
-
 export type Predicate = {
   type: "predicate";
   name: string; // e.g., "Loves", "P"
   args: Term[]; // can be empty for 0-arity predicates
 };
-
 // === Logical Operators ===
-
 export type Not = {
   type: "not";
   operand: Formula;
+  value: '¬'; 
 };
 
 export type And = {
   type: "and";
   left: Formula;
   right: Formula;
+  value: '∧';
 };
 
 export type Or = {
   type: "or";
   left: Formula;
   right: Formula;
+  value: '∨';
 };
 
 export type Implies = {
   type: "implies";
   left: Formula;
   right: Formula;
+  value: '→';
 };
 
 export type Iff = {
   type: "iff";
   left: Formula;
   right: Formula;
+  value: '↔';
 };
 
 export type Equals = {
   type: "equals";
   left: Formula;
   right: Formula;
+  value: '=';
 }
 
 export type NotEquals = {
   type: "notequals";
   left: Formula;
   right: Formula;
+  value: '≠';
 }
 
 export type BinaryConnective = And | Or | Implies | Iff;
 
 // === Quantifiers ===
-
 export type ForAll = {
   type: "forall";
   variable: Variable;
   body: Formula;
+  value: '∀';
 };
 
 export type Exists = {
   type: "exists";
   variable: Variable;
   body: Formula;
+  value: '∃';
 };
 
 export type Quantifier = ForAll | Exists;
-
 // === Formula (well-formed sentence) ===
-
 export type Formula =
   | Predicate
   | Not
