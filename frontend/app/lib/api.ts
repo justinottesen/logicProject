c9caed5ec0cc3fe05cd31d16dced4b0a145d2ae1
Justin Ottesen <justinottesen@gmail.com>
2025-04-21 22:06:00 -0400

    Add API use example to front end

---
+++ frontend/app/lib/api.ts
@@ -0,0 +1,21 @@
+export async function verifyProof(proof: object): Promise<{ valid: true } | { valid: false; step_id: string | null; message: string }> {
+    const res = await fetch("http://localhost:5000/verify_proof", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(proof),
+    });
+  
+    if (res.ok) {
+      return { valid: true };
+    } else {
+      const error = await res.json();
+      return {
+        valid: false,
+        step_id: error.step_id,
+        message: error.message,
+      };
+    }
+  }
+  
